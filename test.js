9405776a589a0f93a29e2ba6c6f2f6ffddec4548
jef king <jef@spud.ca>
2021-02-07 08:07:49 -0800

    testing framework

---
+++ test.js
@@ -0,0 +1,11 @@
+'use strict'
+
+describe('deploy', () => {
+    test('app service', () => {
+        const data = require('./app-service/deploy.json');
+    });
+
+    test('storage account', () => {
+        const data = require('./storage-account/deploy.json');
+    });
+});
