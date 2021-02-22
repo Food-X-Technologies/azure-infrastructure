8b1787af41e553aed0b4497eeb43a56ccf6ec36b
jef king <jef@spud.ca>
2021-02-22 12:13:03 -0800

    tests

--- test.js
+++ test.js
@@ -3,9 +3,11 @@
 describe('deploy', () => {
     test('app service', () => {
         const data = require('./app-service/deploy.json');
+        const params = require('./app-service/params.json');
     });
 
     test('storage account', () => {
         const data = require('./storage-account/deploy.json');
+        const params = require('./storage-account/params.json');
     });
 });
